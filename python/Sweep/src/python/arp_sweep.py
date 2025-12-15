cd6b8ee6513276829685f4610c38fd384454fe4b
Matt Moran <zxsndce@gmail.com>
2025-12-15 17:44:55 -0500

    made ifaces

--- python/Sweep/src/python/arp_sweep.py
+++ python/Sweep/src/python/arp_sweep.py
@@ -1,5 +1,87 @@
-from scapy.all import ARP, Ether, srp, get_if_list, conf
+from scapy.all import *
 import os
+
+PORTS = [80, 443, 21, 22, 53, 25, 110, 143, 993, 3389, 445, 135, 3306, 1433, 123, 139]
+
+def netbios(ip):
+    pkt = IP(dst=ip)/UDP(dport=137)/NBNSQueryRequest(QUESTION_NAME="*")
+    ans = sr1(pkt, timeout=2, verbose=False)
+
+    if ans and ans.haslayer(NBNSQueryResponse):
+        return ans[NBNSQueryResponse].RR_NAME
+
+    return None
+
+def rev_dns(ip):
+    #Dependant on setup
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return None
+
+def mDNS(ip):
+    #Reliable for modern pcs (UDP).
+    port = 5353
+    pkt = (
+        IP(dst=ip)/
+        UDP(sport=RandShort(), dport=5353)/
+        DNS(
+            id=0,
+            qr=0,
+            opcode=0,
+            rd=0,
+            qd=DNSQR(qname="_services._dns-sd._udp.local", qtype="PTR")
+            )
+    )
+
+    resp = sr(pkt, timeout=3, multi=True, verbose=False)[0]
+
+    for _,r in resp:
+        if r.haslayer(DNS):
+            dns = r[DNS]
+            if dns.an:
+                for i in range(dns.ancount):
+                    #return dns.an[i].rrname.decode(errors="ignore")
+                    print(dns.an[i].rrname.decode(errors="ignore"))
+                    exit()
+
+def SMB(ip):
+    #Good for windows
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        s.connect(ip, 445)
+
+        #Minimal SMBv1 negotiate request
+        smb_req = bytes.fromhex(
+            "00000085ff534d427200000000180128000000000000000000000000000000000000000000000000"
+            "000000000000000000000000024e54204c4d20302e313200"
+        )       
+
+        s.send(smb_req)
+        data = s.recv(4096)
+        s.close()
+
+        #Look for ascii hostname:
+        text = data.decode(errors="ignore")
+        for token in text.split("\x00"):
+            if token.isprintable() and len(token) > 3 and token.isupper():
+                print(token)
+    except:
+        pass
+
+def NBNS(ip):
+    #Legacy, outdated
+    pass
+
+# RDNS -> SMB (port:445) -> mDNS -> NBNS
+
+def port_open(ip, port):
+    try:
+        with socket.create_connection((ip, port), timeout=0.5):
+            return True
+    except:
+        return False
 
 #For this to work propely we reqiure NpCap to be installed (Windows)
 NETWORK = os.getenv("network")
@@ -31,3 +113,20 @@ for sent, recv in result:
 print("Avalible Devices:")
 for client in clients:
     print(f"IP:{client["ip"]}\tMAC:{client["mac"]}")
+    print("Open Ports: ")
+    for port in PORTS:
+        if port_open(client["ip"], port):
+            print(port, end="")
+    print()
+
+
+
+"""
+
+getting names:;
+
+Resolve-Dnsname -Name <ip>
+
+(Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName <ip>).Name
+Write-Output 
+"""
---
+++ python/Sweep/src/python/port_sweep.py
@@ -0,0 +1,114 @@
+"""
+
+TARGET IP FULL SCAN:
+
+writing and clearing line:
+sys.stdout.write('\x1b[1A') #Move cursor up 1
+sys.stdout.write('\x1b[2k') #Clear line
+sys.stdout.write('content')
+sys.stdout.flush() #put on screen??
+
+
+May not get results using only scapy since these are raw. windows blocks unsolicited packets.
+so, use socket connect instead:
+
+"""
+from scapy.all import *
+import socket, sys
+from datetime import datetime
+
+TARGET = input("Enter the target IP: ")
+
+PORTS = [80, 443, 21, 22, 53, 25, 110, 143, 993, 3389, 445, 135, 3306, 1433, 123, 139]
+
+NETWORK = os.getenv("network")
+INTERFACE = os.getenv("ETHER_IFACE")
+#INTERFACE = os.getenv("WIFI_IFACE")
+print("Network: ", repr(NETWORK))
+print("Interface: ", repr(INTERFACE))
+
+conf.iface = INTERFACE
+
+def get_mac(ip):
+    pkt= Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip) #Broadcast
+
+    ans = srp(pkt, timeout = 2, verbose = False)[0]
+
+    if ans:
+        return ans[0][1].hwsrc
+    return None
+
+def tcp_connect(ip, port):
+    #Try socket connection
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    print("\tTrying TCP Connection...", end="")
+    try:
+        s.connect((ip,port))
+        s.close()
+        return True
+    except Exception as e:
+        print(f"\t{e}\t")
+        return False
+
+def ping(ip):
+    reply = sr1(IP(dst=ip)/ICMP(), timeout=2, verbose=False)
+    if reply:
+        return True
+    return False
+
+def netbios(ip):
+    pkt = IP(dst=ip)/UDP(dport=137)/NBNSQueryRequest(QUESTION_NAME="*")
+    ans = sr1(pkt, timeout=2, verbose=False)
+
+    if ans and ans.haslayer(NBNSQueryResponse):
+        return ans[NBNSQueryResponse].RR_NAME
+
+    return None
+#Scan for open ports
+def syn_scan(ip, ports):
+    start_time = datetime.now()
+    port_count = len(ports)
+
+    open_ports = []
+    for p in ports:
+        pkt = IP(dst=ip)/TCP(dport=p, flags="S")
+        ans = sr1(pkt, timeout=0.5, verbose=False)
+
+        #sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+        sys.stdout.write('\r') #Return to start of line
+        sys.stdout.write(str(p))
+        sys.stdout.flush()
+        if ans and ans.haslayer(TCP):
+            flags = ans.getlayer(TCP).flags
+            if flags == 0x12: #SYN/ACK
+                open_ports.append(p)
+                #Send rst to be polite?
+                send(IP(dst=ip)/TCP(dport=p, flags="R"), verbose=False)
+
+        #else:
+        #    #If no good, try a direct socket connection, gets passed firewalls
+        #    if tcp_connect(ip, p):
+        #        open_ports.append(p)
+
+    
+    sys.stdout.write('\n')
+
+    return open_ports
+
+mac = get_mac(TARGET)
+print("MAC: ", mac)
+d_name = netbios(TARGET)
+if d_name:
+    print("Device Name: ", d_name)
+print("\nPinging device...")
+got_reply = ping(TARGET)
+print("Reply Received: ", got_reply)
+
+if got_reply:
+    print("Checking for open ports...")
+    open_ports = syn_scan(TARGET, PORTS)
+    print("Open ports:")
+    for port in open_ports:
+        print("\t", port)
