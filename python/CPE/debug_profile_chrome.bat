f0d2ae1de34c6a550f7e5a29ce18273ba0624b93
Matt Moran <zxsndce@gmail.com>
2026-02-04 17:37:39 -0500

    More work on cpe

--- python/CPE/debug_profile_chrome.bat
+++ python/CPE/debug_profile_chrome.bat
@@ -1,9 +1,9 @@
 
 ::Kill Other instances
-taskkill /IM chrome.exe /F
+::taskkill /IM chrome.exe /F
 
 cd "C:\Program Files\Google\Chrome\Application"
 :: Launch chrome in debug mode as this profile:
-chrome.exe --remote-debugging-port=9222 --headless --profile-directory="Profile 1"
+chrome.exe --remote-debugging-port=9222 --remote-allow-origins=* --profile-directory="Profile 2"
 
 ::Note, there is a lock on profiles for normal mode, headless overrides this.
--- python/CPE/src/CDP.py
+++ python/CPE/src/CDP.py
@@ -15,7 +15,7 @@ def main():
 		resp = requests.get("http://localhost:9222/json")
 
 	except:
-		print("Could Not Connect")
+		print("Could Not Get Json.")
 		return
 
 	tabs = resp.json()
@@ -28,86 +28,77 @@ def main():
 
 	ws = websocket.create_connection(ws_url)
 
-	#Send example command to change page:
+	#Enable Autofill:
+	cmd = {"id": 1,"method": "Autofill.enable","params": {}}
+	ws.send(json.dumps(cmd))
+	resp = getResp(ws, 1)
+
+	#Allow pages to send notifications/popups:
+	cmd = {"id": 2,"method": "Page.enable","params": {}}
+	ws.send(json.dumps(cmd))
+	resp = getResp(ws, 2)
+
+	#Navigate to a new page
+	cmd = {"id": 3, "method": "Page.navigate",
+		"params": {"url":"https://id.manulife.ca/"}
+	}
+	ws.send(json.dumps(cmd))
+
+	#Now we listen for a new popup...
+	cnt = 0
+	while True:
+		cnt += 1
+		resp = json.loads(ws.recv())
+
+		if "method" in resp and not "id" in resp:
+				print("== Got event! ==")
+				
+				print("method: ", resp["method"])
+				print("params: ", resp["params"])
+
+		if cnt > 100:
+			break
+	exit()
+
+	#Try Autofill
 	cmd = {
-		"id" : 1, #Unique for each command
-		"method": "Page.navigate",
-		"params": {
-			"url":"https://lmi3d.com/account/?destination=https://www.google.com/"
+	"id": 6,
+	"method":"Autofill.trigger",
+	"params": {
+		"fieldId": user_id["result"]["nodeId"]
 		}
 	}
-
-	#Send it:
-	ws.send(json.dumps(cmd))
-
-	#Read Response:
-	resp = ws.recv()
-	#print(resp)
-
-	#Get Page Tree Frame:
-	cmd = {
-		"id" : 2, #Unique for each command
-		"method": "Page.getFrameTree",
-		"params": {
-			"url":"https://lmi3d.com/account/?destination=https://www.google.com/"
-		}
-	}
-
-	#Send it:
-	ws.send(json.dumps(cmd))
-
-	#Read Response:
-	resp = ws.recv()
-
-	print("DOM.ENABLE:")
-
-	#Enable DOM domain?
-	cmd = {"id" : 3, "method": "DOM.enable"}
-	ws.send(json.dumps(cmd))
-	resp = ws.recv()
-
-	# Get document root of a frame
-	cmd = {"id": 4, "method": "DOM.getDocument", "params": {"depth": -1, "pierce": True}}
-	ws.send(json.dumps(cmd))
-	response = ws.recv()
-	doc = json.loads(response)
-
-	#Next
-	cmd = {
-    "id": 5,
-    "method": "DOM.querySelectorAll",
-    "params": {
-        "nodeId": doc["result"]["root"]["nodeId"],
-        "selector": "input, textarea"
-        }
-    }
 
 	ws.send(json.dumps(cmd))
 
 	while True:
-		msg = json.loads(ws.recv())
-
-		#Ignore Events:
-		if "method" in msg:
-			continue
-
-		if msg.get("id") == 5:
-			node_ids = msg["result"]["nodeIds"]
+		resp = json.loads(ws.recv())
+		if resp.get("id") == 6:
 			break
 
-	print(node_ids)
-	exit()
-
-	resp = ws.recv()
 	print(resp)
 
-	inputs = json.loads(resp)["result"]["nodeIds"]
-
-	print(inputs)
 
 	#close Connection:
 	ws.close()
 
+
+
+def runCommands():
+	for id in range(len(commands) - 1):
+		runCommand(id + 1, commands[id])
+
+def runCommand(id, value):
+	method, params = value
+	cmd = {"id": id, "method": method, "params": params}
+	ws.send(json.dumps(cmd))
+
+def getResp(ws, id):
+	while True:
+		resp = json.loads(ws.recv())
+		if resp.get("id") == id:
+			break
+	return resp
 
 
 def PrintOpenTabs():
